4482e0f3b55f7fabbd12d259b44f74245f9074b1
soyung sim <simsy1024@gmail.com>
2024-08-04 00:30:30 +0900

    PhoneViewController

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,16 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let defalutsNumber = Observable.just("010")
+    
+    let dispose = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +24,39 @@ class PhoneViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
     
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
     }
 
+    func bind(){
+        defalutsNumber
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: dispose)
+        
+        let number = phoneTextField
+            .rx
+            .text
+            .map {$0?.count ?? 0 >= 10}
+        number
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: dispose)
+        
+        number
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .black : .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: dispose)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("버튼 눌림")
+            }
+            .disposed(by: dispose)
+    }
     
     func configureLayout() {
         view.addSubview(phoneTextField)
