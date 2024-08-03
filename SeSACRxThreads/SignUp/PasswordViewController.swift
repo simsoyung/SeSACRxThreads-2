aa99e91db95b7b576f442d0129905d4150518058
soyung sim <simsy1024@gmail.com>
2024-08-03 23:32:16 +0900

    password view

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -45,6 +45,13 @@ class PasswordViewController: UIViewController {
             .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
             .disposed(by: disposeBag)
         
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .systemPink: .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
         nextButton.rx.tap
             .bind(with: self) { owner, _ in
                 print("다음 버튼 누름")
@@ -55,6 +62,7 @@ class PasswordViewController: UIViewController {
     func configureLayout() {
         view.addSubview(passwordTextField)
         view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
          
         passwordTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
@@ -65,6 +73,11 @@ class PasswordViewController: UIViewController {
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(passwordTextField.snp.bottom)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
